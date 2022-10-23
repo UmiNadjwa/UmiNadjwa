217804ec9f0dd8d1d0a809dcd46638a77a7a0a1b
UmiNadjwa <115910532+UmiNadjwa@users.noreply.github.com>
2022-10-23 11:44:16 +0800

    Update iris_dataset.py

--- iris_dataset.py
+++ iris_dataset.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
@@ -6,40 +7,14 @@ iris = sns.load_dataset('iris') # returns a pandas dataframe
 import pandas as pd
 
 
+X_iris = iris.drop('species', axis=1) 
+y_iris = iris['species']
+
+
 xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
 clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
-print('Iris dataset')
-print('Accuracy of RBF SVC classifier on training set: {:.2f}'
+st.write('Iris dataset')
+st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(xtrain, ytrain)))
-print('Accuracy of RBF SVC classifier on test set: {:.2f}'
+st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(xtest, ytest)))
-
-import numpy as np
-import seaborn as sns
-import pandas as pd
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-#from sklearn.datasets import load_iris
-from sklearn.metrics import classification_report
- 
-alist = [1, 2, 3, 4, 5]   # Define a python list. It looks like an np array
-narray = np.array([1, 2, 3, 4]) # Define a numpy array
- 
-print(narray + narray)
-print(alist + alist)
-
-#npmatrix1 = np.array([narray, narray, narray]) # Matrix initialized with NumPy arrays
-#npmatrix2 = np.array([alist, alist, alist]) # Matrix initialized with lists
-#npmatrix3 = np.array([narray, [1, 1, 1, 1], narray]) # Matrix initialized with both types
- 
-#print(npmatrix1)
-#print(npmatrix2)
-#print(npmatrix3)
-
-npmatrix1 = np.array([narray, narray, narray]) # Matrix initialized with NumPy arrays
-npmatrix2 = np.array([alist, alist, alist]) # Matrix initialized with lists
-npmatrix3 = np.array([narray, [1, 1, 1, 1], narray]) # Matrix initialized with both types
- 
-print(npmatrix1)
-print(npmatrix2)
-print(npmatrix3)
